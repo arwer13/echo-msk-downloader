e264d458b0b467e0f33be97e28d526d990b4af9c
ArWer <arwer13@gmail.com>
2015-09-24 21:27:06 +0300

    Some programmes added.

--- config.py
+++ config.py
@@ -21,5 +21,35 @@ programmes = [
         "name": "Суть событий",
         "time": datetime.time(21, 7),
         "dow": Dow.Friday
+    },
+    {
+        "code": "Diletanti",
+        "name": "Дилетанты",
+        "time": datetime.time(21, 7),
+        "dow": Dow.Thursday
+    },
+    {
+        "code": "netak",
+        "name": "Не так",
+        "time": datetime.time(12, 8),
+        "dow": Dow.Sunday
+    },
+    #{
+        #"code": "tabel",
+        #"name": "Табель о рангах",
+        #"time": datetime.time(12, 8),
+        #"dow": Dow.Sunday
+    #},
+    {
+        "code": "48minut",
+        "name": "48 минут",
+        "time": datetime.time(22, 7),
+        "dow": Dow.Wednesday
+    },
+    {
+        "code": "49minut",
+        "name": "49 минут",
+        "time": datetime.time(22, 7),
+        "dow": Dow.Wednesday
     }
 ]
